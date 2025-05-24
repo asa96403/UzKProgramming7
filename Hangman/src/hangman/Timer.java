87d0cc457eb1505dcb759693b263fdfa78175a06
aabert <aabert@smail.uni-koeln.de>
2025-05-24 14:52:35 +0200

    initial setup task 2

---
+++ Hangman/src/hangman/Timer.java
@@ -0,0 +1,12 @@
+package hangman;
+
+public class Timer extends Game{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected Thread timerThread;
+	protected boolean timerRunning;
+	// TODO: Override startGame () to also start the timer
+	// TODO: Implement startTimer () to create and start the timer thread
+}
---
+++ Hangman/src/hangman/TimerRunnable.java
@@ -0,0 +1,19 @@
+package hangman;
+
+import javax.swing.SwingUtilities;
+
+public class TimerRunnable implements Runnable {
+	private final Timer game;
+
+	public TimerRunnable(Timer game) {
+		this.game = game;
+	}
+
+	@Override
+	public void run() {
+// TODO: Implement the countdown loop
+// - Decrease timeLeft every second
+// - Update the time label using SwingUtilities.invokeLater ()
+// - End the game if time runs out
+	}
+}
